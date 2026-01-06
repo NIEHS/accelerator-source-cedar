01da11415712fa4aee6b597449b50240f14baadb
conwaymc <mike.conway@nih.gov>
2026-01-06 08:02:23 -0500

    fixes for cafe ingest

--- accelerator_source_cedar/tests/test_cedar_crosswalk.py
+++ accelerator_source_cedar/tests/test_cedar_crosswalk.py
@@ -54,6 +54,39 @@ class TestAccelCrosswalk(unittest.TestCase):
         actual = accel_cedar_crosswalk.transform(ingest_payload)
         self.assertIsNotNone(actual)
 
+    def test_crosswalk_key_dataset2_via_temp_file(self):
+        temp_dirs_path = "test_resources/temp_dirs"
+        runid = "test_crosswalk_key_dataset"
+        item_id = "test_crosswalk_key_dataset_item"
+        path = os.path.join(temp_dirs_path, runid)
+
+        if os.path.exists(path):
+            shutil.rmtree(path)
+
+        xcom_props_resolver = DirectXcomPropsResolver(temp_files_supported=True, temp_files_location=temp_dirs_path)
+        xcom_utils = XcomUtils(xcom_props_resolver)
+
+        ingest_source_descriptor = IngestSourceDescriptor()
+        ingest_source_descriptor.ingest_identifier = "test"
+        ingest_source_descriptor.ingest_item_id = item_id
+        ingest_source_descriptor.ingest_identifier = runid
+        ingest_source_descriptor.submitter_name = "submitter name"
+        ingest_source_descriptor.submitter_email = "submitter@email"
+        ingest_source_descriptor.schema_version = "1.0.2"
+        ingest_payload = IngestPayload(ingest_source_descriptor)
+
+        ingest_payload.payload_inline = False
+
+        with open("test_resources/key_dataset2.json", 'r') as f:
+            contents_json = json.loads(f.read())
+            stored_path = xcom_utils.store_dict_in_temp_file(item_id, contents_json, runid)
+            ingest_payload.payload_path.append(stored_path)
+
+        accel_cedar_crosswalk = CedarToAccelCrosswalk(xcom_props_resolver)
+        actual = accel_cedar_crosswalk.transform(ingest_payload)
+        self.assertIsNotNone(actual)
+
+
     def test_crosswalk_geo_spatial(self):
         runid = "test_crosswalk_geo_spatial"
         item_id = "test_crosswalk_geo_spatial_item"
