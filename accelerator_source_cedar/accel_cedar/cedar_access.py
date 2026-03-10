8dbe0f6a102f06a615aa164f67954ea0c26b5c00
conwaymc <mike.conway@nih.gov>
2026-03-10 12:16:57 -0400

    fix import order

--- accelerator_source_cedar/accel_cedar/cedar_access.py
+++ accelerator_source_cedar/accel_cedar/cedar_access.py
@@ -8,7 +8,6 @@ import time
 import requests
 
 from accelerator_source_cedar.accel_cedar.cedar_config import CedarConfig
-from pcor_cedar.cedar_access import CedarFolder
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -19,6 +18,33 @@ logger = logging.getLogger(__name__)
 
 base_url = "https://resource.metadatacenter.org"
 template_prefix = "https%3A%2F%2Frepo.metadatacenter.org%2Ftemplate-instances%2F"
+
+
+class CedarFolder():
+
+    def __init__(self, cedar_file_json = None, folder_name=None, folder_id=None, item_type = "folder"):
+        if cedar_file_json:
+            len_path_info = len(cedar_file_json["pathInfo"])
+            if len_path_info > 0:
+                self.folder_name = cedar_file_json["pathInfo"][len_path_info - 1]["schema:name"]
+                self.folder_id = cedar_file_json["pathInfo"][len_path_info - 1]["@id"]
+                self.item_type = "folder"
+            else:
+                raise Exception("No folder found")
+
+            len_subfolders = len(cedar_file_json["resources"])
+            self.subfolders = []
+
+            if len_subfolders > 0:
+                for subfolder in cedar_file_json["resources"]:
+                    self.subfolders.append(CedarFolder(folder_name=subfolder["schema:name"],
+                                                       folder_id=subfolder["@id"], item_type=subfolder["resourceType"]))
+        else:
+            self.folder_name = folder_name
+            self.folder_id = folder_id
+            self.item_type = item_type
+            self.subfolders = []
+
 
 class CedarAccess(object):
 
@@ -131,28 +157,3 @@ class CedarAccess(object):
         x = re.search(re_text, text_to_extract)
         return x.group()
 
-
-class CedarFolder():
-
-    def __init__(self, cedar_file_json = None, folder_name=None, folder_id=None, item_type = "folder"):
-        if cedar_file_json:
-            len_path_info = len(cedar_file_json["pathInfo"])
-            if len_path_info > 0:
-                self.folder_name = cedar_file_json["pathInfo"][len_path_info - 1]["schema:name"]
-                self.folder_id = cedar_file_json["pathInfo"][len_path_info - 1]["@id"]
-                self.item_type = "folder"
-            else:
-                raise Exception("No folder found")
-
-            len_subfolders = len(cedar_file_json["resources"])
-            self.subfolders = []
-
-            if len_subfolders > 0:
-                for subfolder in cedar_file_json["resources"]:
-                    self.subfolders.append(CedarFolder(folder_name=subfolder["schema:name"],
-                                                       folder_id=subfolder["@id"], item_type=subfolder["resourceType"]))
-        else:
-            self.folder_name = folder_name
-            self.folder_id = folder_id
-            self.item_type = item_type
-            self.subfolders = []
